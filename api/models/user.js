4e1ef278976a50fc5be3ca1188300b3f93f43989
NellyNel520 <channelharris520@gmail.com>
2023-07-20 02:20:23 -0400

    added required associations to user modle

--- api/models/user.js
+++ api/models/user.js
@@ -4,14 +4,17 @@ const {
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class User extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
-    }
+
+    
+		static associate(models) {
+			User.hasMany(models.Post,{
+        foreignKey:'userId',
+      })
+      User.hasMany(models.Comment,{
+        foreignKey:'userId',
+        as: 'owner'
+      })
+		}
   }
   User.init({
     username: DataTypes.STRING,
